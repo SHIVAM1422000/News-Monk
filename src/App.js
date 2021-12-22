e27473989ccfe2f89065e6ee64bb6c6c9b04ac9e
SHIVAM1422000 <shivamojha1422000@gmail.com>
2021-12-22 17:12:32 +0530

    Added Navbar Component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 //rcc
 
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Radhey Radhey..!!
+        <Navbar/>
       </div>
     );
   }
