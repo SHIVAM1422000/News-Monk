5e23a72aa18b6928179a14015bc7974b0d9e64d6
SHIVAM1422000 <shivamojha1422000@gmail.com>
2021-12-23 10:51:27 +0530

    Added the functions to loop through the articles from the API call

--- src/components/News.js
+++ src/components/News.js
@@ -1,90 +1,90 @@
-import React, { Component } from 'react';
-import NewsItem from './NewsItem';
-  
-
-
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
-   
-    articles=[
-        {
-        source: {
-        id: "bbc-news",
-        name: "BBC News"
-        },
-        author: "https://www.facebook.com/bbcnews",
-        title: "Indian PM Modi's Twitter hacked with bitcoin tweet",
-        description: "The Indian prime minister's account had a message stating that bitcoin would be distributed to citizens.",
-        url: "https://www.bbc.co.uk/news/world-asia-india-59627124",
-        urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/5998/production/_122063922_mediaitem122063921.jpg"
-        
-    },
-    
+  articles = [
     {
-        source: {
+      source: {
         id: "bbc-news",
-        name: "BBC News"
-        },
-        author: "https://www.facebook.com/bbcnews",
-        title: "Indian PM Modi's Twitter hacked with bitcoin tweet",
-        description: "The Indian prime minister's account had a message stating that bitcoin would be distributed to citizens.",
-        url: "https://www.bbc.co.uk/news/world-asia-india-59627124",
-        urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/5998/production/_122063922_mediaitem122063921.jpg"
-        
+        name: "BBC News",
+      },
+      author: "https://www.facebook.com/bbcnews",
+      title: "Indian PM Modi's Twitter hacked with bitcoin tweet",
+      description:
+        "The Indian prime minister's account had a message stating that bitcoin would be distributed to citizens.",
+      url: "https://www.bbc.co.uk/news/world-asia-india-59627124",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/5998/production/_122063922_mediaitem122063921.jpg",
     },
-    
+
     {
-        source: {
+      source: {
         id: "bbc-news",
-        name: "BBC News"
-        },
-        author: "https://www.facebook.com/bbcnews",
-        title: "Indian PM Modi's Twitter hacked with bitcoin tweet",
-        description: "The Indian prime minister's account had a message stating that bitcoin would be distributed to citizens.",
-        url: "https://www.bbc.co.uk/news/world-asia-india-59627124",
-        urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/5998/production/_122063922_mediaitem122063921.jpg"
-        
+        name: "BBC News",
+      },
+      author: "https://www.facebook.com/bbcnews",
+      title: "Indian PM Modi's Twitter hacked with bitcoin tweet",
+      description:
+        "The Indian prime minister's account had a message stating that bitcoin would be distributed to citizens.",
+      url: "https://www.bbc.co.uk/news/world-asia-india-59627124",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/5998/production/_122063922_mediaitem122063921.jpg",
     },
-    
+
     {
-        source: {
+      source: {
         id: "bbc-news",
-        name: "BBC News"
-        },
-        author: "https://www.facebook.com/bbcnews",
-        title: "Indian PM Modi's Twitter hacked with bitcoin tweet",
-        description: "The Indian prime minister's account had a message stating that bitcoin would be distributed to citizens.",
-        url: "https://www.bbc.co.uk/news/world-asia-india-59627124",
-        urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/5998/production/_122063922_mediaitem122063921.jpg"
-        
+        name: "BBC News",
+      },
+      author: "https://www.facebook.com/bbcnews",
+      title: "Indian PM Modi's Twitter hacked with bitcoin tweet",
+      description:
+        "The Indian prime minister's account had a message stating that bitcoin would be distributed to citizens.",
+      url: "https://www.bbc.co.uk/news/world-asia-india-59627124",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/5998/production/_122063922_mediaitem122063921.jpg",
     },
-    
-    ]
 
+    {
+      source: {
+        id: "bbc-news",
+        name: "BBC News",
+      },
+      author: "https://www.facebook.com/bbcnews",
+      title: "Indian PM Modi's Twitter hacked with bitcoin tweet",
+      description:
+        "The Indian prime minister's account had a message stating that bitcoin would be distributed to citizens.",
+      url: "https://www.bbc.co.uk/news/world-asia-india-59627124",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/5998/production/_122063922_mediaitem122063921.jpg",
+    },
+  ];
 
-    constructor(props){
-        super(props);
-        this.state={
-            article:this.articles,
-            loading:false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      article: this.articles,
+      loading: false,
+    };
+  }
 
+  render() {
+    return (
+      <div>
+        <h1>News Component</h1>
+        <div className="row mx-3 mt-3">
 
-    render() {
-        return (
-            <div>
-                <h1>News Component</h1>
-                <div className="row mx-3 mt-3">
-                    {this.state.article.map(element)=>{}}
-                    <div className="col-md-4">
-                    <NewsItem title={this.state.article[0].title} description={this.state.article[0].description} iurl={this.state.article[0].urlToImage}/>
-                    </div>
-                </div>
-               
-            </div>
-        )
-    }
+          {this.state.article.map((element) => {
+            return(<div className="col-md-4">
+              <NewsItem title={element.title} description={element.description} iurl={element.urlToImage}/>
+            </div>);
+          })};
+
+          
+        </div>
+      </div>
+    );
+  }
 }
 
-export default News
+export default News;
